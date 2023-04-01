e4536745cd56b79f3682094e7d3b96050f6ec331
anoncoderin <113314047+anoncoderin@users.noreply.github.com>
2023-03-31 22:03:23 -0700

    home page second commit

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import NavBar from '@/components/NavBar'
-import GreenButton from '@/components/GreenButton'
-import BrownButton from '@/components/BrownButton'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import NavBar from "@/components/NavBar";
+import GreenButton from "@/components/GreenButton";
+import BrownButton from "@/components/BrownButton";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Seed Sprout</title>
-        <meta name="description" content="Learn how to grow your own fruits and veggies" />
+        <meta
+          name="description"
+          content="Learn how to grow your own fruits and veggies"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <main className={styles.main}>
-        <NavBar/>
-        <GreenButton text='Get Started' link='/' />
-        <BrownButton text='Lorem' link='/' />
-        <BrownButton text='Catalogue' link='/produceCatalogue' />
+        <NavBar />
+        <div className={styles.main_container}>
+          <Image
+            src="/logo/logo-with-text.svg"
+            width={350}
+            height={250}
+            
+          ></Image>
+          <div className={styles.para_main_page}>
+          <p>
+            With Seed Sprout you can learn to grow your own fruits and
+            Vegetables.
+          </p>
+          </div>
+          <div className={styles.home_buttons}>
+            <GreenButton text="Get Started" link="/" />
+            <BrownButton text="About Us" link="/aboutUs" />
+            <BrownButton text="Catalogue" link="/produceCatalogue" />
+          </div>
+        </div>
       </main>
     </>
-  )
+  );
 }
