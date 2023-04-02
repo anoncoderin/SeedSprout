43f326e59c08b6e6da05ab3a3dd19e14cce8ab25
anoncoderin <113314047+anoncoderin@users.noreply.github.com>
2023-04-01 21:02:18 -0700

    result summer-veg-low-largeArea added

--- pages/sumVegSummer.js
+++ pages/sumVegSummer.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/summaryResults.module.css'
+import styles from '@/styles/SumVegSummer.module.css'
 import NavBar from '@/components/NavBar'
 import BrownButton from '@/components/BrownButton'
 
@@ -20,28 +20,31 @@ export default function Results() {
                 <div className={styles.container}>
                     <div>
                         <h1 className={styles.header_results}>Summary</h1>
-                        <Image className={styles.graphic_results} src='summaryGraphics/fruits/resultsFruitsOne.svg' width={327} height={392} />
+                        <Image className={styles.graphic_results} src='VegGraphicImage/vegSummer/result-sum-low-largeArea.svg' width={327} height={392} />
                     </div>
                     <div>
                         <h1 className={styles.header_results}>Results</h1>
-                        <p className={styles.results_description}> These fruits are the most suitable for you to <br></br> grow based on your results. </p>
+                        <p className={styles.results_description}> These vegetables are the most suitable for you to <br></br> grow based on your results. </p>
 
                         <div className={styles.fruit_results}>
                             <a href="/blueberriesResultsPage">
-                                <Image src='/summaryGraphics/fruits/blueberries.svg' width={293} height={136} link="/aboutUs" />
+                                <Image src='VegGraphicImage/vegSummer/corn-low-largeArea.svg' width={293} height={136}/>
                             </a>
                             <a href="/">
-                                <Image src='summaryGraphics/fruits/blackberries.svg' width={293} height={136} />
+                                <Image src='VegGraphicImage/vegSummer/kale-low-largeArea.svg' width={293} height={136} />
                             </a>
                             <a href="/">
-                                <Image src='summaryGraphics/fruits/strawberries.svg' width={293} height={136} />
+                                <Image src='VegGraphicImage/vegSummer/okra-low-largeArea.svg' width={293} height={136} />
                             </a>
                             <a href="/">
-                                <Image src='summaryGraphics/fruits/raspberries.svg' width={293} height={136} />
+                                <Image src='VegGraphicImage/vegSummer/lettuce-low-largeArea.svg' width={293} height={136} />
                             </a>
                         </div>
                     </div>
-                        <BrownButton text='Back' />
+                    <div className={styles.footer_container}>
+                    <Image src='VegGraphicImage/footerGraphicVeg.svg' width={414} height={136} />
+                        <div className={styles.btn_footer}><BrownButton text='Back' /></div>
+                        </div>
                 </div>
             </main>
         </>
