75fa58e67254477191396918b0ed02375c6303ec
anoncoderin <113314047+anoncoderin@users.noreply.github.com>
2023-04-14 13:34:08 -0700

    fixed the coding in resultNew page

--- components/ResultsNew/index.js
+++ components/ResultsNew/index.js
@@ -1,36 +1,37 @@
 import { useState } from "react";
-import Link from "next/link"
-import Image from "next/image"
-import styles from '@/components/ResultsNew/ResultsNew.module.css'
-import { fruitsAndVeggiesCatalogue } from '@/data/fruitsAndVeggiesCatalogue'
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/components/ResultsNew/ResultsNew.module.css";
+import { fruitsAndVeggiesCatalogue } from "@/data/fruitsAndVeggiesCatalogue";
 
+export default function ResultsNew(userinput) {
+  const match = userinput.userinput;
+  console.log("userinput", userinput);
 
-export default function ResultsNew(userinput){
-   const match = userinput.userinput;
- 
-    return(<>
-    
-<div className={styles.whole_result_container}>
-    <h2 className={styles.h_text}>Results  </h2>
-    <p className={styles.p_text}>These vegetables are the most suitable for you to grow based on your results. </p>
-      {
-    fruitsAndVeggiesCatalogue && fruitsAndVeggiesCatalogue.map((item, i)=> (
-        item.index === match &&
-
-<div>
-    <div className={styles.image_selections_bottom}>
-        
-        <div className={styles.list_image}>
-         <Image src={item.img} width={292} height={136}></Image>   
-        </div>
-    </div>
-    
-
-  
-</div>
-
- ))}
-</div>
-
-    </>)
+  return (
+    <>
+      <div className={styles.whole_result_container}>
+        <h2 className={styles.h_text}>Results </h2>
+        <p className={styles.p_text}>
+          These vegetables are the most suitable for you to grow based on your
+          results.{" "}
+        </p>
+        {fruitsAndVeggiesCatalogue &&
+          fruitsAndVeggiesCatalogue.map((item, i) =>
+            // console.log("index",item.index, match, item.index.includes(match)) &&
+            (Array.isArray(item.index)
+              ? item.index.includes(match)
+              : item.index === match) && (
+                  <div>
+                    <div className={styles.image_selections_bottom}>
+                      <div className={styles.list_image}>
+                        <Image src={item.img} width={292} height={136}></Image>
+                      </div>
+                    </div>
+                  </div>
+                )
+          )}
+      </div>
+    </>
+  );
 }
