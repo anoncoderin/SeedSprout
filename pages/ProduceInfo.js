d949b09f531c712b308e2119aba18aad9a79a5b9
jjennbui <113149333+jjennbui@users.noreply.github.com>
2023-04-21 14:54:28 -0700

    renamed test.js

--- pages/ProduceInfo.js
+++ pages/ProduceInfo.js
@@ -1,8 +1,8 @@
-import ResultsInfo from "@/components/resultsInfo";
+import ResultsInfo from "@/components/ResultsInfo";
 import { useRouter } from 'next/router';
 import Blueberries from "@/components/fruits/blueberries"
 
-export default function Test() {
+export default function ProduceInfo() {
   const router = useRouter();
   const { text } = router.query;
 
